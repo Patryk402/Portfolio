cfbac12786a9602f7ef92485e3ec9beb3d884756
Patryk <patryk402@op.pl>
2021-08-16 14:53:48 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const welcome = document.querySelector(".welcome");
+const migacz = document.getElementById("act");
+const strzalka = document.getElementById("znak1");
+const txtWelcome = ['Cześć','Mam na imię Patryk','Zapraszam do opisu'];
+let index = 0;
+let letters = -5;
+
+const startHello = () => {
+    if(letters >= 0){
+        welcome.textContent += txtWelcome[index][letters];
+    }
+
+    letters++;
+
+    if(letters >= txtWelcome[index].length){
+        index++;
+    
+    if(index === txtWelcome.length) return
+    
+    return setTimeout(() => {
+        letters = -5;
+        welcome.textContent = ''
+        startHello();
+    }, 500);
+
+    
+}
+setTimeout(startHello,100);
+}
+ 
+startHello();
+
+// const startHello = ()=> {
+//     welcome.textContent += txtWelcome[index];
+//     index++;
+//     if(index >= txtWelcome.length) clearInterval(przywitanie);
+// }
+
+// const przywitanie =  setInterval(startHello,100);
+
+
+const line = ()=>{
+    migacz.classList.toggle("active");
+}
+setInterval(line,310);
+
+const ani = () =>{
+    strzalka.classList.toggle("dol");
+}
+setInterval(ani,700);

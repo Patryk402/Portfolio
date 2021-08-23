ee1cbddb92fd41e607da5601467e99fba3c006dd
Patryk <patryk402@op.pl>
2021-08-23 21:06:47 +0200

    upgrade website

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const welcome = document.querySelector(".welcome");
 const migacz = document.getElementById("act");
 const strzalka = document.getElementById("znak1");
-const txtWelcome = ['Hello','My name is Patryk','Front-end developer',"let's check out more info about Me in the below"];
+const txtWelcome = ['WITAJ','MAM NA IMIE PATRYK','JESTEM FRONT-END DEVELOPEREM'];
 let index = 0;
 let letters = -5;
 
+// Hello text function
 const startHello = () => {
     if(letters >= 0){
         welcome.textContent += txtWelcome[index][letters];
@@ -39,12 +40,52 @@ startHello();
 // const przywitanie =  setInterval(startHello,100);
 
 
+// teoggle mark in welcome text
 const line = ()=>{
     migacz.classList.toggle("active");
 }
-setInterval(line,310);
+setInterval(line,400);
 
-const ani = () =>{
-    strzalka.classList.toggle("dol");
-}
-setInterval(ani,700);
+//toggle arrow on scroll smooth
+
+// const ani = () =>{
+//     strzalka.classList.toggle("dol");
+// }
+// setInterval(ani,700);
+
+//progress number function
+
+// let nbr = 0;
+// let pokaz;
+// const getNbr = () => {
+
+//     const stop = setInterval(() => {
+//         nbr++;
+//         console.log(nbr);
+//         if(nbr === 70) return clearInterval(stop);
+//     }, 100);
+// }
+
+
+window.addEventListener("scroll", ()=> {
+    
+    if(window.pageYOffset > 510){
+        
+        document.getElementById("htmlBAR").style.width = "70%";
+        document.getElementById("htmlBAR").textContent = "70%";
+        document.getElementById("cssBAR").style.width = "60%";
+        document.getElementById("cssBAR").textContent = "60%";
+        document.getElementById("jsBAR").style.width = "50%";
+        document.getElementById("jsBAR").textContent = "50%";
+        document.getElementById("gitBAR").style.width = "20%";
+        document.getElementById("gitBAR").textContent = "20%";
+        document.getElementById("abt").style.opacity = "1";
+        document.getElementById("abt").style.marginTop ="17%";
+        document.getElementById("abt").style.transition = "2s";
+    } 
+
+});
+
+
+
+
